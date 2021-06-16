a8001146cf88fae97b397d39dfe6c38eed70861f
yanhf <yanhairulfirdan@gmail.com>
2021-06-17 05:40:14 +0700

    add index method for frontController and display product inside index blade file

--- app/Http/Controllers/Ecommerce/FrontController.php
+++ app/Http/Controllers/Ecommerce/FrontController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Ecommerce;
 
 use App\Http\Controllers\Controller;
+use App\Product;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::orderBy('created_at', 'DESC')->paginate(10);
+
+        return view('ecommerce.index', compact('products'));
+    }
 }
