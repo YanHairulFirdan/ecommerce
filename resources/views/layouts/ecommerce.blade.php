39d2c777e9a33cf104280ddabe3e16a14a5c2a3e
yanhf <yanhairulfirdan@gmail.com>
2021-06-17 12:12:07 +0700

    fix js and css call in ecommerce template

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -8,17 +8,17 @@
 
     @yield('title')
 
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/linericon/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/lightbox/simpleLightbox.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/nice-select/css/nice-select.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/animate-css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-ui/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/vendors/linericon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/vendors/owl-carousel/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/vendors/lightbox/simpleLightbox.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/vendors/nice-select/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/vendors/animate-css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/vendors/jquery-ui/jquery-ui.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/css/responsive.css') }}">
 </head>
 
 <body>
@@ -166,28 +166,28 @@
                         <h6 class="footer_title">Instagram Feed</h6>
                         <ul class="list instafeed d-flex flex-wrap">
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-01.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-01.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-02.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-02.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-03.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-03.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-04.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-04.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-05.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-05.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-06.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-06.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-07.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-07.jpg') }}" alt="">
                             </li>
                             <li>
-                                <img src="{{ asset('assets/img/instagram/Image-08.jpg') }}" alt="">
+                                <img src="{{ asset('ecommerce/img/instagram/Image-08.jpg') }}" alt="">
                             </li>
                         </ul>
                     </div>
@@ -228,21 +228,21 @@
     </footer>
     <!--================ End footer Area  =================-->
 
-    <script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ asset('assets/js/popper.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/stellar.js') }}"></script>
-    <script src="{{ asset('assets/vendors/lightbox/simpleLightbox.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/nice-select/js/jquery.nice-select.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/isotope/imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/isotope/isotope-min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.ajaxchimp.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/counter-up/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/flipclock/timer.js') }}"></script>
-    <script src="{{ asset('assets/vendors/counter-up/jquery.counterup.js') }}"></script>
-    <script src="{{ asset('assets/js/mail-script.js') }}"></script>
-    <script src="{{ asset('assets/js/theme.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/popper.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/stellar.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/lightbox/simpleLightbox.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/nice-select/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/isotope/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/isotope/isotope-min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/counter-up/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/flipclock/timer.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/counter-up/jquery.counterup.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/mail-script.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/theme.js') }}"></script>
     @yield('js')
 </body>
 
