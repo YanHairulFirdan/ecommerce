3a71504de77f8e2f38711b9e20cadeb33f6a0d41
yanhf <yanhairulfirdan@gmail.com>
2021-06-17 16:49:30 +0700

    add dashboard method and page for customer

--- app/Http/Controllers/Ecommerce/LoginController.php
+++ app/Http/Controllers/Ecommerce/LoginController.php
@@ -4,11 +4,36 @@ namespace App\Http\Controllers\Ecommerce;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function loginForm()
     {
         return view('ecommerce.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:customers,id',
+            'password' => 'required|string'
+        ]);
+
+
+        $auth = $request->except('_token');
+
+        $auth['status'] = 1;
+
+        if (Auth::guard('customer')->attempt($auth)) {
+            return redirect()->intended('customer.dashboard');
+        }
+
+        return redirect()->back()->with(['error' => 'Email/password salah']);
+    }
+
+    public function dashboard()
+    {
+        return view('ecommerce.dashboard');
     }
 }
